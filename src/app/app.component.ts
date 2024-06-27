502c906ae6b567085848ea22a610875d7ce9e1af
petarbechev <petar.bechev@vasoftware.co.uk>
2024-06-27 16:01:38 +0300

    Added all functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, RequiredValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private httpClient: HttpClient){}
+
+  form?: FormGroup;
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      accessToken: new FormControl('', Validators.required),
+      orderNumber: new FormControl('', Validators.required),
+      amount: new FormControl('',Validators.required),
+    });
+    this.generateData();
+  }
   title = 'captivlink-demo-client';
+
+  generateData(){
+    var orderNumber = makeid(10);
+    var amount = (Math.random() * 150).toFixed(2);
+    this.form?.controls["orderNumber"].setValue(orderNumber);
+    this.form?.controls["amount"].setValue(amount);
+  }
+
+  submit(){
+    this.form?.markAllAsTouched()
+
+    if(this.form?.invalid){
+      return;
+    }
+    var request = this.form?.getRawValue();
+    request.accessToken = null;
+    this.httpClient.post("https://e.captivlink.com/purchase", request, {headers: {"x-access-token": this.form?.controls["accessToken"].value}} ).subscribe(res=>{
+
+    })
+  }
 }
+
+function makeid(length: number) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
